8d0e1e2e24a29a25e3ca1e9e3d1dc952f2bb3697
mtinsonk <mtinsonk@gmail.com>
2022-08-08 02:35:11 +0000

    Created files

--- main.tf
+++ main.tf
@@ -5,3 +5,24 @@ terraform {
   }
  }
 }
+resource "aws_iam_role" "eks-iam-role" {
+ name = "devopsthehardway-eks-iam-role"
+
+ path = "/"
+
+ assume_role_policy = <<EOF
+{
+ "Version": "2012-10-17",
+ "Statement": [
+  {
+   "Effect": "Allow",
+   "Principal": {
+    "Service": "eks.amazonaws.com"
+   },
+   "Action": "sts:AssumeRole"
+  }
+ ]
+}
+EOF
+
+}
